6b084007b83f0eacc5331d3c8fd0cb8205d136ad
omkarb-bhise7 <131230125+omkarb-bhise7@users.noreply.github.com>
2025-05-19 17:27:50 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const REACT_APP_BACKEND_URL=`https://gym-server-k6k4.onrender.com`;
 
 // Member API calls
 export const getMembers = async () => {
